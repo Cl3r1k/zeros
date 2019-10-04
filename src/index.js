40bf30ea26ba97e980479c2e08f73f5a10d312c3
Cl3r1k <Cl3r1k2@gmail.com>
2019-10-04 11:12:46 +0700

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function zeros(expression) {
-  // your solution
+
+    const factors = expression.split('*');
+
+    let result = BigInt(1);
+    factors.map(numStr => {
+        const symbs = numStr.split('!');
+        const num = parseInt(symbs[0], 10);
+        const isDoubleFact = symbs.length > 2;
+
+        const fact = factorial(num, isDoubleFact);
+        result *= fact;
+    });
+
+    const testCase = result.toString();
+    const zerosResult = testCase.match(/[0]+$/gmi);
+
+    let zerosCount = 0;
+    if (zerosResult && zerosResult.length > 0) {
+        zerosCount = zerosResult[0].length;
+    }
+
+    return zerosCount;
+
+};
+
+function factorial(num, isDoubleFact) {
+    let startIndex = BigInt(1);
+    let adjustment = BigInt(1);
+    if (isDoubleFact) {
+        if (num % 2 === 0) {
+            startIndex = BigInt(2);
+        }
+        adjustment = BigInt(2);
+    }
+
+    let factResult = BigInt(1);
+    for (let index = startIndex; index <= BigInt(num); index += adjustment) {
+        factResult *= index;
+    }
+
+    return factResult;
 }
